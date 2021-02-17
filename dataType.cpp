c5d977eca884a28661ce308fb2ce2a3187ac8966
ceverhardz <ceverhardz29@gmail.com>
2021-02-17 13:56:47 +0800

    add dataType code

---
+++ dataType.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x = 10;
+	char gender = 'M';
+	string name = "Katuuk Christian";
+	float hours = 5.17;
+	double gpa = 3.78;
+	bool isActive = false;
+	
+	cout<<name<< endl;
+	
+	return 0;
+	
+}
